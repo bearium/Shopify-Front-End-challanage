24f542e985497f74d761b6eafe962e607a59a133
bearium <jacob_martin@rogers.com>
2018-01-08 13:32:16 -0500

    add js portion, everything working

---
+++ Front_end.js
@@ -0,0 +1,10 @@
+function signup(){
+  var email = document.getElementById("email").value;
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+    console.log("validated Email: " + email + ", Interest: " + document.getElementById("dropdown").value);
+    document.getElementById('error').style.display = "none";
+    return true;
+  }
+  document.getElementById('error').style.display = "block";
+  return false;
+}
