7f3b6a681350b988a6907e417669c637227e9c21
bearium <jacob_martin@rogers.com>
2018-01-08 14:13:11 -0500

    added form

--- Front_end.js
+++ Front_end.js
@@ -1,4 +1,5 @@
 function signup(){
+
   var email = document.getElementById("email").value;
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
     console.log("validated Email: " + email + ", Interest: " + document.getElementById("dropdown").value);
